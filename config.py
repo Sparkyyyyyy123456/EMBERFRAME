e1ea52269d8df142a68da28f02f16079a0ce64e8
Sparkyyyyyy123456 <nishadspark675@gmail.com>
2026-09-25 23:14:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,18 +2,18 @@
 # EMBERFRAME SETTINGS
 
 #PREREQUISITES
-BBOX = (11.601563,-8.928487,29.882813,8.407168) # Input bounding box as per this format (xmin, ymin, xmax, ymax)
-SATELLITE_SOURCES = ['VIIRS_NOAA20_NRT', 'MODIS_NRT', 'VIIRS_SNPP_NRT'] # Input wanted satellite sources for fire data
-MAP_KEY = "7bb86ca838c20a9947edfa4d13304b8a" # Input NASA FIIRMS API key
-GRID_SIZE = 3 # Specify amount of rows and columns the BBOX should be divided into
-DAYS_DATA = 50 # Specify how many days of data should be used
+BBOX = (00, 00, 00, 00) # Input bounding box as per this format (xmin, ymin, xmax, ymax)
+SATELLITE_SOURCES = ['Source 1', 'Source 2', 'Source 3', '....'] # Input wanted satellite sources for fire data
+MAP_KEY = "ENTER API KEY" # Input NASA FIIRMS API key
+GRID_SIZE = 10 # Specify amount of rows and columns the BBOX should be divided into
+DAYS_DATA = 180 # Specify how many days of data should be used
 
 #SENSITIVITY
 PRONE_THRESHOLD = 0.2 # >PRONE_THRESHOLD = Flagged as prone
 HIGHRISK_THRESHOLD = 0.5 # >HIGHRISK_THRESHOLD = Flagged as high risk
 
 #AI PARAMETERS
-HOT_THRESHOLD = 31.8 # Defines above what temperature is considered hot
+HOT_THRESHOLD = 30 # Defines above what temperature is considered hot
 DRY_THRESHOLD = 2.0 # Defines below what precipitation is considered dry
 STD_DEV_LIMIT = 5.0 # Defines the limit of standard deviation for which temperature varies
 
